64067bdc1e3048b6896dd1602d76a41283fa5844
Hussar99 <43067400+Hussar99@users.noreply.github.com>
2018-11-30 16:54:46 +0000

    1st steps in level design and adding movement to the player character.

---
+++ Assets/Scripts/Lava.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour {
+
+    // Unity calls this function automatically
+    // When our enemy touch any other object
+    private void OnCollisionEnter2D(Collision2D collsion)
+    {
+
+        //Check if the thing that we collided with is the player (aka has a Player script)
+        Player PlayerScript = collsion.collider.GetComponent<Player>();
+        // Only do something if the thing we run into was in fact the player (aka playerScript is not null)
+
+        if (PlayerScript != null)
+        {
+            // We DID hit the player!
+
+            //KILL THEM!
+            PlayerScript.Kill();
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Extra using statement to allow us to use the scene managament functions
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 10;
+    public float jumpSpeed = 20;
+    public Rigidbody2D physicsBody;
+    public string horizontalAxis = "Horizontal";
+    public string jumpButton = "Jump";
+
+ 
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Get axis input from Unity
+        float leftRight = Input.GetAxis(horizontalAxis);
+
+        // Create direction vector from axis input
+        Vector2 direction = new Vector2(leftRight, 0);
+
+        // Make direction vector length 1
+        direction = direction.normalized;
+
+        // Calculate the velocity
+        Vector2 velocity = direction * speed;
+        velocity.y = physicsBody.velocity.y;
+
+        // Give the velocity to the rigid body
+        physicsBody.velocity = velocity;
+
+
+        // Jumpoing
+
+        bool jumpButtonPressed = Input.GetButtonDown(jumpButton);
+        if (jumpButtonPressed == true)
+        {
+            // We have pressed jump, so we should set our upward velocity to jumpSpeed
+            velocity.y = jumpSpeed;
+
+            // Give the velocity to rigidbody
+            physicsBody.velocity = velocity;
+
+
+        }
+
+    }
+
+    // Our own function for handling player death
+    public void Kill()
+    {
+        // Reset the current level to reset from the beggining.
+
+        // First, ask unity what the current level is
+        Scene currentLevel = SceneManager.GetActiveScene();
+
+        // Second, tell unity to load the current level again by passing the build index of our level
+        SceneManager.LoadScene(currentLevel.buildIndex);
+
+    }
+
+
+}
